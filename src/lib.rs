aa574f1fd8655f422fbca73a2ff7c254f5c5ca6e
William Berg <william.berg2@gmail.com>
2023-07-23 16:53:23 +0200

    started multithreaded func

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-use std::io::{Write};
+use std::io::Write;
 use std::{error::Error, path::PathBuf};
 use std::path::Path;
 use walkdir::WalkDir;
 use std::fs::{ File, OpenOptions };
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 use std::hash::{Hash, Hasher};
+
 const CACHE_FILE_NAME: &str = "cache";
 const CACHE_SAVE_PATH: &str = "C:\\Users\\willi\\Documents\\GitHub\\fsearch";
 
@@ -56,8 +57,8 @@ impl Hash for CachedFile {
 }
 
 pub fn run(file_to_find: &String) -> Result<(), Box<dyn Error>> {
-    let root_dir = "C:\\"; // Replace with your desired directory
-
+    // let root_dir = "C:\\"; // Replace with your desired directory
+    let root_dir = "C:\\Users\\willi\\Documents\\GitHub\\fsearch";
     let cache_path_part = Path::new(CACHE_SAVE_PATH);
     let cache_path = cache_path_part.join(CACHE_FILE_NAME);
 
@@ -92,10 +93,22 @@ pub fn run(file_to_find: &String) -> Result<(), Box<dyn Error>> {
 // Currently does not work
 fn get_hash_cache(cache_path: &PathBuf) -> Result<HashSet<CachedFile>, Box<dyn Error>> {
     let mut set: HashSet<CachedFile> = HashSet::new();
-    std::fs::read_to_string(cache_path)?.lines()
-                                       .for_each(|line| {
-                                            set.insert(CachedFile::create(line));
-                                       });
+    std::fs::read_to_string(cache_path)?
+        .lines()
+        .for_each(|line| {
+            let cached_file = CachedFile::create(line);
+            if set.contains(&cached_file) {
+                // println!("Already found: {}\nThis time at: {}", cached_file.name, line);
+                let mut taken = set.take(&cached_file).unwrap();
+                let tup = split_path(line);
+                if !taken.paths.contains(&tup.0) {
+                    taken.paths.push(tup.0);
+                }
+                set.insert(taken);
+            } else {
+                set.insert(cached_file);
+            }
+        });
     Ok(set)
 }
 
@@ -130,8 +143,6 @@ fn split_path(path: &str) -> (String, String) {
 }
 
 fn fill_cache(path: String, cache_path: &PathBuf) -> Result<HashSet<CachedFile>, Box<dyn Error>> {
-    let cache_exists = cache_exists(&cache_path)?;
-
     let cache_file_path = cache_path;
 
     // Error handling please!~!!!!
@@ -141,13 +152,6 @@ fn fill_cache(path: String, cache_path: &PathBuf) -> Result<HashSet<CachedFile>,
         .open(&cache_file_path)?;
 
     let mut cache_set: HashSet<CachedFile> = HashSet::new();
-    if cache_exists {
-        std::fs::read_to_string(cache_file_path).unwrap()
-                                                .lines()
-                                                .for_each(| line | {
-                                                    cache_set.insert(CachedFile::create(line));
-                                                });
-    }
     // Recursively iterate over directory contents
     println!("Filling cache...");
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
@@ -167,12 +171,45 @@ fn fill_cache(path: String, cache_path: &PathBuf) -> Result<HashSet<CachedFile>,
             writeln!(cache_file, "{}", path_as_string)?;
             cache_set.insert(cached_file);
         } else {
-            let mut taken = cache_set.take(&cached_file).expect("Something went wrong getting the cached file");
-            if !taken.paths.contains(&path_as_string) {
-                taken.paths.push(path_as_string);
+            let mut taken = cache_set.take(&cached_file)
+                .expect("Something went wrong getting the cached file");
+            let tup = split_path(&path_as_str);
+            if !taken.paths.contains(&tup.0) {
+                writeln!(cache_file, "{}", path_as_string)?;
+                taken.paths.push(tup.0);
             }
             cache_set.insert(taken);
         }
     }
     Ok(cache_set)
+}
+
+fn fill_cache_multi_threaded(path: String, cache_path: &PathBuf) -> Result<HashSet<CachedFile>, Box<dyn Error>> {
+    let dirs = std::fs::read_dir(path)?;
+    let dir_vec: Vec<_> = dirs.collect();
+    let dir_vecdeq: VecDeque<_> = dir_vec.into_iter().collect();
+    while dir_vecdeq.len() < 10 {
+        let curr = dir_vecdeq.pop_front().unwrap().unwrap();
+        let tmp = std::fs::read_dir(curr.path())?.collect::<Vec<>>();
+    }
+
+}
+
+fn get_file_paths(path: &String) -> Vec<String> {
+    let mut return_vec: Vec<String> = vec![];
+    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+            let path = entry.path();
+
+            // Skip if the path is not a file
+            if !path.is_file() {
+                continue;
+            }
+
+            // Writes to cache if file isn't already there
+            let path_as_str = path.as_os_str().to_str().unwrap();
+            let path_as_string = String::from(path_as_str);
+
+            return_vec.push(path_as_string);
+        }
+    return_vec
 }
